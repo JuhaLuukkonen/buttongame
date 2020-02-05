b5ffd4c152eee9d021e92ae950f94cf1fdcf270b
Juha Luukkonen <32368550+JuhaLuukkonen@users.noreply.github.com>
2020-02-05 07:48:00 +0200

    Add files via upload

---
+++ build/precache-manifest.e48c8959fa612c82fbb1231434968d5c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f504571768b3ebd26dadb3a18357c96e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1d8457b8edcdcedefae",
+    "url": "/static/css/main.b09f78dd.chunk.css"
+  },
+  {
+    "revision": "137b41ecc696f04ee6f9",
+    "url": "/static/js/2.01f2a86c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.01f2a86c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1d8457b8edcdcedefae",
+    "url": "/static/js/main.b09f3a35.chunk.js"
+  },
+  {
+    "revision": "b31d548233905fec4ad7",
+    "url": "/static/js/runtime-main.6ce8346b.js"
+  },
+  {
+    "revision": "10a5b071548379871a331ddae24e289f",
+    "url": "/static/media/check.10a5b071.svg"
+  },
+  {
+    "revision": "9661ba0b1e7abb32b6b4b956d5d14899",
+    "url": "/static/media/minus.9661ba0b.svg"
+  },
+  {
+    "revision": "7c6c8212afc5044e4b60f59552e57e69",
+    "url": "/static/media/plus.7c6c8212.svg"
+  },
+  {
+    "revision": "7fad484d3fc1b16b62081c4dfa80d5d9",
+    "url": "/static/media/user-minus.7fad484d.svg"
+  },
+  {
+    "revision": "5047d00469e004c51f6b14e245c66d6f",
+    "url": "/static/media/user-plus.5047d004.svg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/static/media/x.b0200bf7.svg"
+  }
+]);
